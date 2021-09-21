96eb3b885f249054ff2b3782aa4340b4a4525512
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-09-21 14:49:16 +0900

    add  process.exit(1)

--- src/fetch.ts
+++ src/fetch.ts
@@ -104,4 +104,5 @@ const main = async () => {
 
 main().catch(err => {
   console.error(err);
+  process.exit(1);
 });
--- src/userID.ts
+++ src/userID.ts
@@ -7,4 +7,5 @@ const main = async () => {
 
 main().catch(err => {
   console.error(err);
+  process.exit(1);
 });
